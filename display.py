03d2d5be61799af922241f2c18705fcc3fc7e7ab
Thomas Mohaupt <Thomas.Mohaupt@gmail.com>
2020-01-02 21:28:40 +0100

    init

---
+++ display.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2017 Adafruit Industries
+# Author: Tony DiCola & James DeVito
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import time
+import Adafruit_SSD1306
+import subprocess
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from pytz import timezone
+from dateutil.parser import parse
+
+# Raspberry Pi pin configuration:
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# 128x64 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+
+disp.begin()
+
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+lineheight = 10
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Load default font.
+font = ImageFont.load_default()
+
+while True:
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d\' \' -f1"
+    IP = subprocess.run(cmd, shell = True, encoding = 'UTF-8', capture_output=True ).stdout
+    cmd = "top -bn1 | grep load | awk '{printf \"C: %.2f\", $(NF-2)}'"
+    CPU = subprocess.run(cmd, shell = True, encoding = 'UTF-8', capture_output=True ).stdout
+    cmd = "free -m | awk 'NR==2{printf \"M: %.0f%%\", $3*100/$2 }'"
+    MemUsage = subprocess.run(cmd, shell = True, encoding = 'UTF-8', capture_output=True ).stdout
+    cmd = "df -h | awk '$NF==\"/\"{printf \"D: %s\", $5}'"
+    Disk = subprocess.run(cmd, shell = True, encoding = 'UTF-8', capture_output=True ).stdout
+    cmd = "uptime| sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* users?.*//; s/days/d/; s/ ?([[:digit:]]+):0?([[:digit:]]+)/\\1 h, \\2 m/'"
+    Up = subprocess.run(cmd, shell = True, encoding = 'UTF-8', capture_output=True ).stdout
+    cmd = "grep 'temperature,location=ttnbox' /var/log/sensors.log | tail -1 | cut -d ' ' -f2| cut -d = -f2"
+    T_in = subprocess.run(cmd, shell = True, encoding = 'UTF-8', capture_output=True ).stdout
+    cmd = "grep 'temperature,location=attic' /var/log/sensors.log | tail -1 | cut -d ' ' -f2| cut -d = -f2"
+    T_out = subprocess.run(cmd, shell = True, encoding = 'UTF-8', capture_output=True ).stdout
+
+    cmd = "curl -s http://noc.thethingsnetwork.org:8085/api/v2/gateways/eui-b827ebfffe06902a | jq '.timestamp'"
+    TTNts = parse(subprocess.run(cmd, shell = True, encoding = 'UTF-8', capture_output=True ).stdout)
+    TTNts = TTNts.astimezone(timezone('Europe/Berlin'))
+
+    # Write text
+    y = top
+    draw.text((x, y), "Up: " + str(Up),  font=font, fill=255)
+    y += lineheight
+    draw.text((x, y), "IP: " + str(IP),  font=font, fill=255)
+    y += lineheight
+    draw.text((x, y), str(CPU) + " " + str(MemUsage) + " " + str(Disk), font=font, fill=255)
+    y += lineheight
+    draw.text((x, y), "Box: " + str(T_in) + u"\u2103  Loft: " + str(T_out) + u"\u2103",  font=font, fill=255)
+    y += lineheight
+    draw.text((x, y), "TTN: last seen " + str(TTNts),  font=font, fill=255)
+
+    # Display image
+    disp.image(image)
+    disp.display()
+    time.sleep(0.1)
---
+++ fetchsensors.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import time
+import sys
+import paho.mqtt.client as mqtt
+import json
+import smbus
+
+MQTT_SERVER = 'mqtt.thomo.de'
+
+# Data capture and upload interval in seconds. Less interval will eventually hang the DHT22.
+INTERVAL = 20
+
+TOPIC = 'tmp'
+PAYLOAD = ("{},location={},node={},sensor={} value={:.2f}")
+ERRLOAD = ("error,location={},node={},sensor={} type={},value={}")
+
+sensors = (
+    { 
+        'id': '28-0301a279dec6', 
+        'sensor': 'DS18B20', 
+        'node': 'ttn-gateway', 
+        'location': 'ttnbox', 
+        'values': [ 
+            { 'correction': -1.1, 'measurand': 'temperature', 'raw': 0.0},
+        ], 
+        'error': {},
+    }, 
+    {
+        'id': '28-0301a2794002', 
+        'sensor': 'DS18B20', 
+        'node': 'ttn-gateway', 
+        'location': 'attic', 
+        'values': [ 
+            { 'correction': -0.2, 'measurand': 'temperature', 'raw': 0.0},
+        ],
+        'error': {},
+    },
+    {
+        'id': 0x40, 
+        'sensor': 'Si7021', 
+        'node': 'ttn-gateway', 
+        'location': 'attic', 
+        'values': [ 
+            { 'correction': -1.2, 'measurand': 'temperature', 'raw': 0.0},
+            { 'correction': 7.0, 'measurand': 'humidity', 'raw': 0.0}, 
+        ],
+        'error': {},
+    }
+)
+
+def readDS18B20(sensor): 
+    try:
+        file = open('/sys/bus/w1/devices/'+sensor['id']+'/w1_slave')
+        filecontent = file.read()
+        file.close()
+    
+        tp = filecontent.split("\n")[1].split(" ")[9]
+        sensor['values'][0]['raw'] = float(tp[2:]) / 1000
+
+        sensor['error'] = {}
+    except:
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        sensor['error'] = { 'type': exc_type, 'value': exc_value }
+
+def readSI7021(bus, sensor):
+    i2caddr = sensor['id']
+
+    try:
+        hm = bus.read_i2c_block_data(i2caddr, 0xE5, 2) 
+        time.sleep(0.1)
+        sensor['values'][1]['raw'] = ((hm[0] * 256 + hm[1]) * 125 / 65536.0) - 6
+
+        tp = bus.read_i2c_block_data(i2caddr, 0xE3, 2)
+        time.sleep(0.1)
+        sensor['values'][0]['raw'] = ((tp[0] * 256 + tp[1]) * 175.72 / 65536.0) - 46.85
+
+        sensor['error'] = {}
+    except:
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        sensor['error'] = { 'type': exc_type, 'value': exc_value }
+
+next_reading = time.time() 
+
+client = mqtt.Client()
+
+# Set access token
+# client.username_pw_set(ACCESS_TOKEN)
+
+# Connect to ThingsBoard using default MQTT port and 60 seconds keepalive interval
+client.connect(MQTT_SERVER, 1883, 60)
+
+client.loop_start()
+
+i2cbus = smbus.SMBus(1)
+
+try:
+    while True:
+        for item in sensors:
+            if item['sensor'] == 'DS18B20':
+                readDS18B20(item) 
+            elif item['sensor'] == 'Si7021':
+                readSI7021(i2cbus, item)
+            else:
+                # ignore
+                pass
+            if not item['error']:
+                for v in item['values']:
+                    # pass
+                    print(PAYLOAD.format(v['measurand'],item['location'],item['node'],item['sensor'],v['raw']+v['correction']))
+                    client.publish(TOPIC, PAYLOAD.format(v['measurand'],item['location'],item['node'],item['sensor'],v['raw']+v['correction']))
+            else:
+                print(ERRLOAD.format(item['location'],item['node'],item['sensor'],item['error']['type'],item['error']['value']), file=sys.stderr)
+                client.publish(TOPIC, ERRLOAD.format(item['location'],item['node'],item['sensor'],item['error']['type'],item['error']['value']))
+
+        next_reading += INTERVAL
+        sleep_time = next_reading-time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+except KeyboardInterrupt:
+    pass
+
+
+client.loop_stop()
+client.disconnect()
