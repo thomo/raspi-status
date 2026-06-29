4323c1390dc2837762eebb8bf1e9fc564e44cfa9
Thomas Mohaupt <thomas.mohaupt@gmail.com>
2026-06-29 14:35:03 +0200

    update sensorprobe

--- sensorprobe/go.mod
+++ sensorprobe/go.mod
@@ -3,7 +3,6 @@ module sensorprobe
 go 1.18
 
 require (
-	github.com/d2r2/go-dht v0.0.0-20200119175940-4ba96621a218
 	github.com/d2r2/go-i2c v0.0.0-20191123181816-73a8a799d6bc
 	github.com/gookit/config/v2 v2.2.8
 	github.com/influxdata/influxdb-client-go/v2 v2.14.0
@@ -13,7 +12,6 @@ require (
 	dario.cat/mergo v1.0.2 // indirect
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/d2r2/go-logger v0.0.0-20210606094344-60e9d1233e22 // indirect
-	github.com/d2r2/go-shell v0.0.0-20211022052110-f591c27e3e2e // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-viper/mapstructure/v2 v2.5.0 // indirect
 	github.com/google/uuid v1.3.1 // indirect
--- sensorprobe/src/sensorprobe.go
+++ sensorprobe/src/sensorprobe.go
@@ -1,105 +1,99 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "time"
+	"context"
+	"log"
+	"time"
 
-    "github.com/gookit/config/v2"
-    "github.com/gookit/config/v2/properties"
-    "github.com/d2r2/go-i2c"
-    influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	goi2c "github.com/d2r2/go-i2c"
+	"github.com/gookit/config/v2"
+	"github.com/gookit/config/v2/properties"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 )
 
 type Config struct {
-    InfluxURL   string `mapstructure:"influxURL"`
-    InfluxToken string `mapstructure:"influxToken"`
-    InfluxOrg   string `mapstructure:"influxOrg"`
-    InfluxBucket string `mapstructure:"influxBucket"`
+	InfluxURL    string `mapstructure:"influxURL"`
+	InfluxToken  string `mapstructure:"influxToken"`
+	InfluxOrg    string `mapstructure:"influxOrg"`
+	InfluxBucket string `mapstructure:"influxBucket"`
 }
 
 func loadConfig(filename string) (*Config, error) {
-    config.WithOptions(config.ParseEnv)
-    config.AddDriver(properties.Driver)
+	config.WithOptions(config.ParseEnv)
+	config.AddDriver(properties.Driver)
 
-    if err := config.LoadFiles(filename); err != nil {
-        return nil, err
-    }
+	if err := config.LoadFiles(filename); err != nil {
+		return nil, err
+	}
 
-    var cfg Config
-    if err := config.BindStruct("", &cfg); err != nil {
-        return nil, err
-    }
+	var cfg Config
+	if err := config.BindStruct("", &cfg); err != nil {
+		return nil, err
+	}
 
-    return &cfg, nil
+	return &cfg, nil
 }
 
 func readI2CSensor() (float64, error) {
-    i2c, err := i2c.NewI2C(0x48, 1)
-    if err != nil {
-        return 0, err
-    }
-    defer i2c.Close()
+	conn, err := goi2c.NewI2C(0x48, 1)
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
 
-    // Replace with actual sensor reading logic
-    data, err := i2c.ReadBytes(2)
-    if err != nil {
-        return 0, err
-    }
+	// Replace with actual sensor reading logic
+	buf := make([]byte, 2)
+	_, err = conn.ReadBytes(buf)
+	if err != nil {
+		return 0, err
+	}
 
-    // Convert data to a meaningful value
-    value := float64(data[0]) + float64(data[1])/256.0
-    return value, nil
+	value := float64(buf[0]) + float64(buf[1])/256.0
+	return value, nil
 }
 
 func read1WireSensor() (float64, error) {
-    // Replace with actual 1-Wire sensor reading logic
-    value, err := dht.ReadDHTxxWithRetry(dht.DHT22, "GPIO_PIN", false, 10)
-    if err != nil {
-        return 0, err
-    }
-    return value, nil
+	// Replace with actual 1-Wire sensor reading logic
+	return 0, nil
 }
 
-func sendToInfluxDB(client influxdb2.Client, measurement string, value float64) error {
-    writeAPI := client.WriteAPIBlocking(influxOrg, influxBucket)
-    p := influxdb2.NewPoint(measurement,
-        map[string]string{"unit": "value"},
-        map[string]interface{}{"value": value},
-        time.Now())
-    return writeAPI.WritePoint(p)
+func sendToInfluxDB(client influxdb2.Client, cfg *Config, measurement string, value float64) error {
+	writeAPI := client.WriteAPIBlocking(cfg.InfluxOrg, cfg.InfluxBucket)
+	p := influxdb2.NewPoint(measurement,
+		map[string]string{"unit": "value"},
+		map[string]interface{}{"value": value},
+		time.Now())
+	return writeAPI.WritePoint(context.Background(), p)
 }
 
 func main() {
-    config, err := loadConfig("config.properties")
-    if err != nil {
-        log.Fatalf("Error loading config: %v", err)
-    }
+	cfg, err := loadConfig("config.properties")
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
 
-    client := influxdb2.NewClient(influxURL, influxToken)
-    defer client.Close()
+	client := influxdb2.NewClient(cfg.InfluxURL, cfg.InfluxToken)
+	defer client.Close()
 
-    for {
-        i2cValue, err := readI2CSensor()
-        if err != nil {
-            log.Printf("Error reading I2C sensor: %v", err)
-        } else {
-            err = sendToInfluxDB(client, "i2c_sensor", i2cValue)
-            if err != nil {
-                log.Printf("Error sending I2C data to InfluxDB: %v", err)
-            }
-        }
+	for {
+		i2cValue, err := readI2CSensor()
+		if err != nil {
+			log.Printf("Error reading I2C sensor: %v", err)
+		} else {
+			if err = sendToInfluxDB(client, cfg, "i2c_sensor", i2cValue); err != nil {
+				log.Printf("Error sending I2C data to InfluxDB: %v", err)
+			}
+		}
 
-        oneWireValue, err := read1WireSensor()
-        if err != nil {
-            log.Printf("Error reading 1-Wire sensor: %v", err)
-        } else {
-            err = sendToInfluxDB(client, "1wire_sensor", oneWireValue)
-            if err != nil {
-                log.Printf("Error sending 1-Wire data to InfluxDB: %v", err)
-            }
-        }
+		oneWireValue, err := read1WireSensor()
+		if err != nil {
+			log.Printf("Error reading 1-Wire sensor: %v", err)
+		} else {
+			if err = sendToInfluxDB(client, cfg, "1wire_sensor", oneWireValue); err != nil {
+				log.Printf("Error sending 1-Wire data to InfluxDB: %v", err)
+			}
+		}
 
-        time.Sleep(20 * time.Second)
-    }
+		time.Sleep(20 * time.Second)
+	}
 }
